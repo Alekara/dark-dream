509f6d9557873c075584603f5690268a6d1eb6e2
Alekara <43556614+Alekara@users.noreply.github.com>
2018-11-18 16:17:19 +0300

    сделано полностью все, кроме галереи

---
+++ script.js
@@ -0,0 +1,13 @@
+var leftArrow = document.querySelector(".btn-arrow--left");
+var rightArrow = document.querySelector(".btn-arrow--left");
+
+var item1 = document.querySelector(".why-us__item--1");
+var item2 = document.querySelector(".why-us__item--2");
+var item3 = document.querySelector(".why-us__item--3");
+var item4 = document.querySelector(".why-us__item--4");
+var item5 = document.querySelector(".why-us__item--5");
+var item6 = document.querySelector(".why-us__item--6");
+var item7 = document.querySelector(".why-us__item--7");
+
+var items = [item1, item2, item3, item4, item5, item6, item7];
+var current = 0;
