e51ad837cb4706796417ad9c9a825d2662184802
Alekara <43556614+Alekara@users.noreply.github.com>
2018-11-19 02:17:09 +0300

    доделала галерею в мобильных версиях, уменьшила футер

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 var leftArrow = document.querySelector(".btn-arrow--left");
-var rightArrow = document.querySelector(".btn-arrow--left");
+var rightArrow = document.querySelector(".btn-arrow--right");
 
-var item1 = document.querySelector(".why-us__item--1");
-var item2 = document.querySelector(".why-us__item--2");
-var item3 = document.querySelector(".why-us__item--3");
-var item4 = document.querySelector(".why-us__item--4");
-var item5 = document.querySelector(".why-us__item--5");
-var item6 = document.querySelector(".why-us__item--6");
-var item7 = document.querySelector(".why-us__item--7");
-
-var items = [item1, item2, item3, item4, item5, item6, item7];
 var current = 0;
+var items = document.querySelectorAll(".gallery__photo");
+
+
+rightArrow.addEventListener("click", clickRightArrow);
+leftArrow.addEventListener("click", clickLeftArrow);
+
+
+function clickRightArrow() {
+  items[current].style.display = "none";
+  if (current == items.length - 1) {
+    current = 0;
+  } else {
+    ++current;
+  }
+  items[current].style.display = "block";
+  return current;
+};
+
+function clickLeftArrow() {
+  items[current].style.display = "none";
+  if (current == 0) {
+    current = items.length - 1;
+  } else {
+    --current;
+  }
+  items[current].style.display = "block";
+  return current;
+};
